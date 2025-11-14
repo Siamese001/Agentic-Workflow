c91971561bc4c499747f42ce46ea57d7fa2d8470
Siamese001 <amitayer1@gmail.com>
2025-11-14 05:28:10 -0500

    Delete chromadb directory

--- chromadb/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-class embedding_functions:  # pragma: no cover - stub namespace
-    class EmbeddingFunction:
-        def __call__(self, *args, **kwargs):  # pragma: no cover - interface stub
-            raise NotImplementedError
