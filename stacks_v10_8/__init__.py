91482a97b4c46a57d139a35fcff53bd48f6440fe
Siamese001 <amitayer1@gmail.com>
2025-11-15 05:50:43 -0500

    refactor: add v10.8 pure execution stacks

--- stacks_v10_8/__init__.py
+++ stacks_v10_8/__init__.py
@@ -1,11 +1,17 @@
-"""v10.8 planning stacks used for deterministic L1 cognition."""
+"""v10.8 planning + execution stacks used across the workflow."""
 
 from .rag_planning import RAGPlanningStack
 from .bullet_planning import BulletPlanningStack
 from .draft_planning import DraftPlanningStack
+from .rag_execution import RAGExecutionStack
+from .bullet_execution import BulletExecutionStack
+from .drafting_execution import DraftingExecutionStack
 
 __all__ = [
     "RAGPlanningStack",
     "BulletPlanningStack",
     "DraftPlanningStack",
+    "RAGExecutionStack",
+    "BulletExecutionStack",
+    "DraftingExecutionStack",
 ]
