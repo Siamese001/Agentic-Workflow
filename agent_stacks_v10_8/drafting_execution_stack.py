cf64eaecb0052fee6769466250cfb8e1ceaf56b5
Siamese001 <amitayer1@gmail.com>
2025-11-16 07:28:38 -0500

    Delete agent_stacks_v10_8 directory

--- agent_stacks_v10_8/drafting_execution_stack.py
+++
@@ -1,5 +0,0 @@
-"""Compatibility shim that re-exports the v10.8 Draft execution stack."""
-
-from stacks_v10_8.drafting_execution import DraftingExecutionStack
-
-__all__ = ["DraftingExecutionStack"]
