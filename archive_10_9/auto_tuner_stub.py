d079b44ad60c207d66470bfdbc710c11071595bf
Siamese001 <amitayer1@gmail.com>
2025-11-18 14:36:06 -0500

    Delete archive_10_9 directory

--- archive_10_9/auto_tuner_stub.py
+++
@@ -1,8 +0,0 @@
-class PolicyAutoTunerStub:
-    def suggest_config(self, state, metrics):
-        # deterministic suggestion stub
-        return {
-            "temperature": 0.3,
-            "max_tokens": 500,
-            "routing_adjustment": "none",
-        }
