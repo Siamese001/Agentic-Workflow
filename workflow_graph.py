b5f81429a56620b3bd9ade58777e8d205f3097b7
Amit <amitayer1@gmail.com>
2025-11-24 12:30:14 -0500

    import fixes

--- workflow_graph.py
+++ workflow_graph.py
@@ -51,8 +51,7 @@ from runtime.observability import (
 from orchestration.dag_engine import Node as DagNode, Edge as DagEdge, Graph as DagGraph, DAGExecutor
 from core.agent_registry import AgentRegistry
 from profiles.agent_profile import AgentCard
-from core.agent_router_policy import choose_agents_for_task
-from l2 import (
+from l2.execution import (
     _execute_strategy,
     _execute_retrieval,
     _execute_drafting,
