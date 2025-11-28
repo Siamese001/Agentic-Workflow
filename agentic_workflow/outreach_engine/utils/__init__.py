64df1852806f9eaf4d6d57995118f9624b6ff2d6
Amit <amitayer1@gmail.com>
2025-11-28 16:25:39 -0500

    merge

---
+++ agentic_workflow/outreach_engine/utils/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""
+Outreach Engine Utilities - LinkedIn outreach specific utilities
+"""
+
+__all__: list[str] = []
--- agentic_workflow/services/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-"""
-Services Layer - Resume Generator v10_12
-Service interfaces for the RG engine
-"""
-
-__all__: list[str] = []
