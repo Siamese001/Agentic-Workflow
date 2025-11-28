3dc8f0b3940574a916030d0238135b7b41f754d1
Amit <amitayer1@gmail.com>
2025-11-28 08:57:04 -0500

    LIC functionality

---
+++ tests/L3_orchestration/test_lic_orchestrator_structure.py
@@ -0,0 +1,14 @@
+import inspect
+from agentic_workflow.L3_orchestration.lic_orchestrator import LICOrchestrator
+
+def test_orchestrator_class_exists():
+    assert inspect.isclass(LICOrchestrator)
+
+def test_required_methods_exist():
+    methods = [
+        "run", "run_k1", "run_k2", "run_k3", "run_k4",
+        "run_k5", "run_k6", "run_k7",
+        "apply_safety_checks", "should_retry"
+    ]
+    for m in methods:
+        assert hasattr(LICOrchestrator, m)
