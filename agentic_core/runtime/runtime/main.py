d8d70655daaa2a14e55b1c6cbd2bf178fa77e544
Amit <amitayer1@gmail.com>
2025-11-30 09:50:14 -0500

    modules

---
+++ agentic_core/runtime/runtime/main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Main entry point for Resume Generator v10_12
+Provides the primary public API for resume generation
+"""
+
+from runtime import generate_resume_v10_12
+
+__all__ = ['generate_resume_v10_12']
