c2a4a37ad70a1f2640ae68ead55b9b88bc85144d
Siamese001 <amitayer1@gmail.com>
2025-12-07 16:50:14 -0500

    Align SSoT validator and SSoT YAML with numbered domain roots

--- 09_apps/apps_lic/L1_cognition/P2_inspect/business_ops/find_msg_problems/__init__.py
+++ 09_apps/apps_lic/L1_cognition/P2_inspect/business_ops/find_msg_problems/__init__.py
@@ -1,7 +1,7 @@
 """
-09_apps/apps_lic/L1_cognition/P2_inspect/business_ops/find_msg_problems package initialization.
+09_apps/apps_lic/L1_cognition/P2_inspect/business_ops/find_msg_problems/diagnostics_ops package initialization.
 
-Generated: 2025-12-07T13:28:54.058609
+Generated: 2025-12-07T13:28:54.057959
 """
 
 from __future__ import annotations
--- 09_apps/apps_rg/L1_cognition/P2_inspect/business_ops/vectorization_ops/embedding_compare_meaning/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-09_apps/apps_rg/L1_cognition/P2_inspect/business_ops/vectorization_ops/embedding_compare_meaning package initialization.
-
-Generated: 2025-12-07T13:28:54.208625
-"""
-
-from __future__ import annotations
-
-__all__: list[str] = []
