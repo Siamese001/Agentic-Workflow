065f059cd8cb14674927266795636d0b206f15fd
Siamese001 <amitayer1@gmail.com>
2025-12-10 10:23:49 -0500

    deleted directories

--- config/logic/validation/find_problems/diagnostics/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-05_config.logic.validation.find_problems.diagnostics_ops — Package initialization
-
-"""
-
-__all__: list = []
--- config/runtime/guardrails/check_rules/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-05_config.runtime_ops.guardrails.check_rules — Package initialization
-
-"""
-
-__all__: list = []
