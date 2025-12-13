f4986f3087f30c322f225217c87e3e89fc1eea53
Siamese001 <amitayer1@gmail.com>
2025-12-13 06:52:42 -0500

    cleanup

--- apps_rg/L2_execution/achv_bullet_synthesizer_types.py
+++ apps_rg/L2_execution/achv_bullet_synthesizer_types.py
@@ -9,7 +9,7 @@ violated the Subatomic Canon. It has been refactored into focused submodules.
 """
 
 # Re-export all components for backward compatibility
-from .achv_bullet_synthesizer_types_enums import *
-from .achv_bullet_synthesizer_types_models import *
+from .achv_enums import *
+from .achv_models import *
 
 __all__ = ['*']  # Re-export all imported names
