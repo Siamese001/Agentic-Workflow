ed87db733f962b711c343ad907d5193f08f73283
Siamese001 <amitayer1@gmail.com>
2025-12-29 10:58:03 -0500

    cv

--- agentic_core/semantic_memory/embedding_logic/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Embedding Logic Module - Sovereign Primary
-Provides standardized embedding generation for semantic memory operations.
-"""
-
-from .core_embedder import CoreEmbedder, get_embedding
-
-__all__ = ["CoreEmbedder", "get_embedding"]
