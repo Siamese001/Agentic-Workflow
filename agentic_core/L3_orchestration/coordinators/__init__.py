8fd504bc79b1129c31b3fb98dac832dfd4feb03e
Siamese001 <amitayer1@gmail.com>
2026-01-07 12:50:13 -0500

    phase 5 ssot cleanup

--- agentic_core/L3_orchestration/coordinators/__init__.py
+++
@@ -1,5 +0,0 @@
-"""L3 Orchestration Coordinators - Consolidated workflow coordinators."""
-from .rl_coordinator import RLCoordinator
-from .recovery_coordinator import RecoveryCoordinator
-
-__all__ = ['RLCoordinator', 'RecoveryCoordinator']
