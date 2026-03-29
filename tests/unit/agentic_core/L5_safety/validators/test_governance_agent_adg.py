0205667cd18d65ddd1226e2dd82fd18bce4f61c5
Siamese001 <siamese001@users.noreply.github.com>
2026-03-29 04:54:12 -0400

    Fix 2 syntax errors in _adg.py test files

--- tests/unit/agentic_core/L5_safety/validators/test_governance_agent_adg.py
+++ tests/unit/agentic_core/L5_safety/validators/test_governance_agent_adg.py
@@ -69,7 +69,7 @@ class TestGovernanceAgentSource:
 
     # Assert
     assert result is not None, "Function should return a result"
-    assert isinstance(result, (dict, list, str, int, float, bool)), "Result should be a common type", "Result should be an object"
+    assert isinstance(result, (dict, list, str, int, float, bool)), "Result should be a common type"
     # TODO: Add specific runtime behavior assertions
     def test_heal_references_manual_required(self):
         assert "manual_required" in _src_text()
