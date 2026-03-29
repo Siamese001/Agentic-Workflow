cddad4580c942b032c906a9fac0e05342897f523
Siamese001 <siamese001@users.noreply.github.com>
2026-03-29 06:30:57 -0400

    Wave 4: Fix E2E and Integration Tests - 473 tests collected, 0 errors

--- apps_rfp/engines/__init__.py
+++ apps_rfp/engines/__init__.py
@@ -1,5 +1,5 @@
 """apps_rfp/engines -- RFP engines."""
 
-from apps_rfp.engines.base_rfp_engine import BaseRFPEngine
+from apps_rfp.engines.base_rfp_engine import BaseRfpEngine
 
-__all__ = ["BaseRFPEngine"]
+__all__ = ["BaseRfpEngine"]
