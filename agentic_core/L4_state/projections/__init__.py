a4e6799d7dd11a9d8276c56ea9fd3979aee5d63f
Siamese001 <amitayer1@gmail.com>
2026-07-12 14:25:59 -0400

    feat(l4): add projection package

---
+++ agentic_core/L4_state/projections/__init__.py
@@ -0,0 +1,9 @@
+"""Outbox-driven L4 read-surface projections."""
+
+from agentic_core.L4_state.projections.runner import (
+    ProjectionHandler,
+    ProjectionRunResult,
+    ProjectionRunner,
+)
+
+__all__ = ["ProjectionHandler", "ProjectionRunResult", "ProjectionRunner"]
